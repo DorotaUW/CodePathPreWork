855a512a0244a1f59e01a441e40baa7dc7f54ed1
DorotaUW <dpasek@uw.edu>
2018-07-12 16:57:40 -0700

    Update allFactors.java

--- allFactors.java
+++ allFactors.java
@@ -1,3 +1,13 @@
+/*
+Given a number N, find all factors of N.
+
+Example: N = 6 
+factors = {1, 2, 3, 6}
+
+Make sure the returned array is sorted.
+*/
+
+// Returns a sorted list of factors of n
 public ArrayList<Integer> allFactors(int n) 
 {
     ArrayList<Integer> list = new ArrayList<>();
