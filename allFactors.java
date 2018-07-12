574b5fc197b36a689a7852738d060cd556f92705
DorotaUW <dpasek@uw.edu>
2018-07-12 16:53:35 -0700

    Update allFactors.java

--- allFactors.java
+++ allFactors.java
@@ -1,20 +1,21 @@
-public ArrayList<Integer> allFactors(int n) {
-       ArrayList<Integer> list = new ArrayList<>();
-        if(n == 1 || n == 0)
-        {
-            list.add(n);            
-            return list;
-        }
-        for(int i = (int)Math.sqrt(n); i >= 1; i--)
-        {
-            if(n % i == 0)
-            {
-                list.add(0,i);
-                if(i != Math.sqrt(n))
-                {
-                    list.add(n/i);
-                }
+public ArrayList<Integer> allFactors(int n) 
+{
+    ArrayList<Integer> list = new ArrayList<>();
+    if(n == 1 || n == 0)
+    {
+       list.add(n);            
+       return list;
+    }
+    for(int i = (int)Math.sqrt(n); i >= 1; i--)
+    {
+       if(n % i == 0)
+       {
+           list.add(0,i);
+           if(i != Math.sqrt(n))
+           {
+                list.add(n/i);
             }
         }
-        return list;
-    }
+     }
+     return list;
+}
